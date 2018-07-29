72567a502513cfd78b396b9ae8d4408defc3166b
Rob Taussig <robert.taussig@gmail.com>
2018-07-28 23:44:49 -0400

    remove unused dependencies

--- components/Controls.jsx
+++ components/Controls.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Typography from '@material-ui/core/Typography';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputLabel from '@material-ui/core/InputLabel';
 import MobileDetect from 'mobile-detect';
 const md = new MobileDetect(window.navigator.userAgent);
 const isMobile = Boolean(md.mobile());
